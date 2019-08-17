9cde3cd9db5e521719e82ccca6446a399896ac78
visual_community@outlook.com <visual_community@outlook.com>
2019-08-17 13:40:02 -0400

    Solucion completa

---
+++ MVCfood/MVCfood/Models/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using System.Data.Entity;
+
+namespace MVCfood.Models
+{
+    public class DataContext:DbContext
+    {
+        public DataContext():base("DefaultConnection")
+        {
+
+        }
+
+        public System.Data.Entity.DbSet<MVCfood.Models.Food> Foods { get; set; }
+    }
+}
---
+++ MVCfood/MVCfood/Models/Food.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVCfood.Models
+{
+    public class Food
+    {
+        [Key]
+        public int foodID { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Display(Description ="Food description")]
+        [StringLength(30,ErrorMessage ="Te pasaste choco solo 30 caracteres", MinimumLength =5)]
+        public string Description { get; set; }
+
+
+    }
+}
